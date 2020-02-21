148f4012dba1bfaf799c4f1f54f212e98907ebfa
Alexandre CASTANET <30659286+acastanet@users.noreply.github.com>
2020-02-21 16:15:12 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -61,20 +61,22 @@ namespace ESP8266ThingSpeak {
     * Connect to ThingSpeak and upload data. It would not upload anything if it failed to connect to Wifi or ThingSpeak.
     */
     //% block="Upload data to ThingSpeak|URL/IP = %ip|Write API key = %write_api_key|Field 1 = %n1|Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
-    //% ip.defl=api.thingspeak.com
+    //% ip.defl=5.135.153.79
     //% write_api_key.defl=your_write_api_key
     export function connectThingSpeak(ip: string, write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
         if (wifi_connected && write_api_key != "") {
             thingspeak_connected = false
-            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",80", 0) // connect to website server
-            thingspeak_connected = waitResponse()
+            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",1880", 0) // connect to website server
+            //thingspeak_connected = waitResponse()
+            thingspeak_connected = true
             basic.pause(100)
             if (thingspeak_connected) {
                 last_upload_successful = false
                 let str: string = "GET /update?api_key=" + write_api_key + "&field1=" + n1 + "&field2=" + n2 + "&field3=" + n3 + "&field4=" + n4 + "&field5=" + n5 + "&field6=" + n6 + "&field7=" + n7 + "&field8=" + n8
                 sendAT("AT+CIPSEND=" + (str.length + 2))
                 sendAT(str, 0) // upload data
-                last_upload_successful = waitResponse()
+                //last_upload_successful = waitResponse()
+                last_upload_successful = true
                 basic.pause(100)
             }
         }
